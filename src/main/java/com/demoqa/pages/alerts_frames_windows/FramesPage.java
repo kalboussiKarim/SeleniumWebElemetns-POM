afbb9f36b97b66a05d5afac27f938f7854738af6
kalboussi karim <kalboussikarim3@gmail.com>
2024-12-22 00:29:14 +0100

    Switching to, and testing iFrames (using Index)

--- src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
+++ src/main/java/com/demoqa/pages/alerts_frames_windows/FramesPage.java
@@ -2,14 +2,14 @@ package com.demoqa.pages.alerts_frames_windows;
 
 import org.openqa.selenium.By;
 
-import static utilities.SwitchToUtility.switchToDefaultContent;
-import static utilities.SwitchToUtility.switchToFrameString;
+import static utilities.SwitchToUtility.*;
 
 public class FramesPage extends Alerts_Frames_WindowsPage{
 
     private By textInFrame = By.id("sampleHeading");
     private By headerFramesText = By.xpath("//div[@id='framesWrapper']//h1[text()='Frames']");
     private String iFrameBigBox = "frame1";
+
 
     public String getHeaderFramesText(){
         return find(headerFramesText).getText();
@@ -19,11 +19,23 @@ public class FramesPage extends Alerts_Frames_WindowsPage{
         switchToFrameString(iFrameBigBox);
     }
 
+    private void switchToSmallBox(){
+        switchToFrameIndex(2);
+    }
+
     public String getTextInBigFrame(){
         switchToBigBox();
         String bigFrameText = find(textInFrame).getText();
-        System.out.println("Big Frame Text :"+ bigFrameText);
+        //System.out.println("Big Frame Text :"+ bigFrameText);
         switchToDefaultContent();
         return bigFrameText;
+    }
+
+    public String getTextInSmallFrame(){
+        switchToSmallBox();
+        String smallFrameText = find(textInFrame).getText();
+        //System.out.println("Small Frame Text :"+ smallFrameText);
+        switchToDefaultContent();
+        return smallFrameText;
     }
 }
--- src/test/java/demo/qa/tests/tests/frames/FramesTest.java
+++ src/test/java/demo/qa/tests/tests/frames/FramesTest.java
@@ -5,14 +5,25 @@ import demo.qa.tests.base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Test
 public class FramesTest extends BaseTest {
 
-    @Test
+
     public void testFramesBigBox(){
         FramesPage framesPage = homePage.goToAlertsFramesWindowsCard().clickFramesMenuItem();
         String actualBigBoxText = framesPage.getTextInBigFrame();
         String expectedBigBoxText = "This is a sample page";
         Assert.assertEquals(actualBigBoxText,expectedBigBoxText,"\n Actual & Expected Text Do Not Match\n");
+        String actualHeaderText = framesPage.getHeaderFramesText();
+        String expectedHeaderText = "Frames";
+        Assert.assertEquals(actualHeaderText,expectedHeaderText,"\n Actual & Expected Header Text Do Not Match \n");
+    }
+
+    public void testFramesSmallBox(){
+        FramesPage framesPage = homePage.goToAlertsFramesWindowsCard().clickFramesMenuItem();
+        String actualSmallBoxText = framesPage.getTextInSmallFrame();
+        String expectedSmallBoxText = "This is a sample page";
+        Assert.assertEquals(actualSmallBoxText, expectedSmallBoxText,"\n Actual & Expected Text Do Not Match\n");
         String actualHeaderText = framesPage.getHeaderFramesText();
         String expectedHeaderText = "Frames";
         Assert.assertEquals(actualHeaderText,expectedHeaderText,"\n Actual & Expected Header Text Do Not Match \n");
