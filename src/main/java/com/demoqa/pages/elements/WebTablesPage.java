4593c0f93fedb132a65fb20eca3c464e937546b2
kalboussi karim <kalboussikarim3@gmail.com>
2024-12-29 16:53:14 +0100

    updated readmi.md file

--- src/main/java/com/demoqa/pages/elements/WebTablesPage.java
+++ src/main/java/com/demoqa/pages/elements/WebTablesPage.java
@@ -12,11 +12,11 @@ public class WebTablesPage extends ElementsPage{
     private By submitButton = By.id("submit");
 
     public void setEditButtonEmail(String email){
-        editButton = By.xpath("//div[text()='"+email+"']//following::span[@title='Edit']");
+        editButton = By.xpath("//div[text()='"+email+"']/following::span[@title='Edit']");
     }
     //div[text()='alden@example.com']//preceding::div[1]
     public void setAgeFieldEmail(String email){
-        ageField = By.xpath("//div[text()='"+email+"']//preceding::div[1]");
+        ageField = By.xpath("//div[text()='"+email+"']/preceding::div[1]");
     }
     public String getTableAge(){
         return find(ageField).getText();
